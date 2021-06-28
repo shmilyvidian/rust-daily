38c6388c0ecf4fa3834fb21a2a07cfc732b78a5e
shmilyvidian <shmilyvidian@163.com>
2021-06-28 23:03:44 +0800

    feat: 增加match Option

--- _enum/src/main.rs
+++ _enum/src/main.rs
@@ -21,6 +21,23 @@ fn main() {
 
     let home = IpAddr::V4(127,0,0,1);
     let loopback = IpAddr::V6(String::from("::1"));
-    let m = Message::Write(String::from("hello")).call();
-    println!("m is {}", m);
+    
+    let some_number = Some(1);
+    // None必须声明类型，因为编译器无法自动推导
+    let none_number:Option<i32> = None;
+    println!("some_number: {}", some_number.unwrap());
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 => println!("four"),
+        _ => (),
+    }
+    let some_value = Some(0);
+    if let Some(0) = some_value {
+        println!("bingo")
+    } else {
+        println!("not match")
+    }
 }
